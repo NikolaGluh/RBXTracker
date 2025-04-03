5f4fc5143a9d08ce2b732e976377caf2296bca22
Nik <gluhovicnikola66@gmail.com>
2025-04-03 14:55:51 +0200

    some minor fixes and debugging code

--- chrome/load.js
+++ chrome/load.js
@@ -1,34 +1,40 @@
-function waitForElm(selector) {
-  return new Promise(resolve => {
-    if (document.querySelector(selector)) resolve(document.querySelector(selector));
-
-    const observer = new MutationObserver(() => {
-      if (document.querySelector(selector)) {
-        resolve(document.querySelector(selector));
-        observer.disconnect();
-      }
+if (window.RBXTracker_Loaded) {
+  console.warn("Tracker already loaded");
+} else {
+  window.RBXTracker_Loaded = true;
+  
+  function waitForElm(selector) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) resolve(document.querySelector(selector));
+  
+      const observer = new MutationObserver(() => {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+  
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
     });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  });
+  }
+  
+  (async () => {
+    const div = document.createElement('div');
+    div.id = 'rbxt-panel';
+    div.innerHTML = await fetch(chrome.runtime.getURL('panel.html')).then(res => res.text());
+  
+    if (document.body.classList.contains('dark-theme')) div.classList.add('dark');
+  
+    const linebreak = document.createElement('br');
+  
+    const runningGames = await waitForElm('#rbx-running-games');
+  
+    runningGames.parentNode.insertBefore(div, runningGames);
+    runningGames.parentNode.insertBefore(linebreak, runningGames);
+  })();
+  
+  console.log("Tracker loaded #2")
 }
-
-(async () => {
-  const div = document.createElement('div');
-  div.id = 'rbxt-panel';
-  div.innerHTML = await fetch(chrome.runtime.getURL('panel.html')).then(res => res.text());
-
-  if (document.body.classList.contains('dark-theme')) div.classList.add('dark');
-
-  const linebreak = document.createElement('br');
-
-  const runningGames = await waitForElm('#rbx-running-games');
-
-  runningGames.parentNode.insertBefore(div, runningGames);
-  runningGames.parentNode.insertBefore(linebreak, runningGames);
-})();
-
-console.log("Tracker loaded #2")
